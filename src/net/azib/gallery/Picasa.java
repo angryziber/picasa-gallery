6deab8d8a6323fbb06aa9695683354640eb6a9bf
Anton Keks <anton@codeborne.com>
2011-12-25 18:41:37 +0200

    use plain http for performance

--- src/net/azib/gallery/Picasa.java
+++ src/net/azib/gallery/Picasa.java
@@ -24,12 +24,12 @@ public class Picasa {
     }
 
     public List<AlbumEntry> getAlbums() {
-        UserFeed feed = feed("https://picasaweb.google.com/data/feed/api/user/" + USER + "?kind=album", UserFeed.class);
+        UserFeed feed = feed("http://picasaweb.google.com/data/feed/api/user/" + USER + "?kind=album", UserFeed.class);
         return feed.getAlbumEntries();
     }
 
     public AlbumFeed getAlbum(String name) {
-        return feed("https://picasaweb.google.com/data/feed/api/user/" + USER + "/album/" + name + "?imgmax=1024&thumbsize=144c", AlbumFeed.class);
+        return feed("http://picasaweb.google.com/data/feed/api/user/" + USER + "/album/" + name + "?imgmax=1024&thumbsize=144c", AlbumFeed.class);
     }
 
     private <T extends IFeed> T feed(String url, Class<T> type) {
