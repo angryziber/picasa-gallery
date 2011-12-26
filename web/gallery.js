8bd600612a74c8f4ddbccf85af243a7360a9ddf0
Anton Keks <anton@codeborne.com>
2011-12-26 20:57:21 +0200

    advance to next/prev photos with swipes on touch devices

--- web/gallery.js
+++ web/gallery.js
@@ -57,6 +57,10 @@ Shadowbox.init({
     viewportPadding: 5,
     onOpen: function(e) {
         if (history.pushState) history.pushState(stateURL(e), e.link.title, stateURL(e));
+        $('#sb-container').touchwipe({
+            wipeLeft: Shadowbox.next,
+            wipeRight: Shadowbox.previous
+        });
     },
     onChange: function(e) {
         if (history.replaceState) history.replaceState(stateURL(e), e.link.title, stateURL(e));
@@ -78,3 +82,73 @@ function doSearch() {
     goto('/' + $('#search').val());
     return false;
 }
+
+(function($) {
+    $.fn.touchwipe = function(settings) {
+        var config = {
+            min_move_x: 20,
+            min_move_y: 20,
+            wipeLeft: function() {},
+            wipeRight: function() {},
+            wipeUp: function() {},
+            wipeDown: function() {},
+            preventDefaultEvents: true
+        };
+
+        if (settings) $.extend(config, settings);
+
+        this.each(function() {
+            var startX;
+            var startY;
+            var isMoving = false;
+
+            function cancelTouch() {
+                this.removeEventListener('touchmove', onTouchMove);
+                startX = null;
+                isMoving = false;
+            }
+
+            function onTouchMove(e) {
+                if (config.preventDefaultEvents) {
+                    e.preventDefault();
+                }
+                if (isMoving) {
+                    var x = e.touches[0].pageX;
+                    var y = e.touches[0].pageY;
+                    var dx = startX - x;
+                    var dy = startY - y;
+                    if (Math.abs(dx) >= config.min_move_x) {
+                        cancelTouch();
+                        if (dx > 0)
+                            config.wipeLeft();
+                        else
+                            config.wipeRight();
+                        isMoving = false;
+                    }
+                    else if (Math.abs(dy) >= config.min_move_y) {
+                        cancelTouch();
+                        if (dy > 0)
+                            config.wipeDown();
+                        else
+                            config.wipeUp();
+                        isMoving = false;
+                    }
+                }
+            }
+
+            function onTouchStart(e) {
+                if (e.touches.length == 1) {
+                    startX = e.touches[0].pageX;
+                    startY = e.touches[0].pageY;
+                    isMoving = true;
+                    this.addEventListener('touchmove', onTouchMove, false);
+                }
+            }
+
+            if ('ontouchstart' in document.documentElement)
+                this.addEventListener('touchstart', onTouchStart, false);
+        });
+
+        return this;
+    };
+})(jQuery);
