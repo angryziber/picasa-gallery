72d3153eeb9259453e0f90bc702ab8f1cc108548
Anton Keks <anton@codeborne.com>
2011-12-27 21:07:15 +0200

    improve caching

--- src/net/azib/photos/RequestRouter.java
+++ src/net/azib/photos/RequestRouter.java
@@ -1,5 +1,8 @@
 package net.azib.photos;
 
+import com.google.gdata.data.BaseFeed;
+import com.google.gdata.data.photos.UserFeed;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,26 +17,27 @@ public class RequestRouter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) resp;
-
-        String agent = request.getHeader("User-Agent");
         String path = request.getRequestURI();
 
         if (path == null || "/".equals(path)) {
-            request.setAttribute("gallery", picasa.getGallery());
-            render("gallery", request, response);
+            render("gallery", picasa.getGallery(), request, response);
         }
         else {
             String[] parts = path.split("/");
-            request.setAttribute("album", picasa.getAlbum(parts[1]));
             request.setAttribute("photoId", parts.length > 2 ? parts[2] : null);
-            render("album", request, response);
+            render("album", picasa.getAlbum(parts[1]), request, response);
         }
     }
 
-    void render(String jsp, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    void render(String template, BaseFeed feed, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         request.setAttribute("picasa", picasa);
+        request.setAttribute(template, feed);
+
         response.setContentType("text/html; charset=utf8");
-        request.getRequestDispatcher("/WEB-INF/jsp/" + jsp + ".jsp").include(request, response);
+        response.addDateHeader("Last-Modified", feed.getUpdated().getValue());
+        response.addHeader("ETag", feed.getEtag());
+
+        request.getRequestDispatcher("/WEB-INF/jsp/" + template + ".jsp").include(request, response);
     }
 
     public void destroy() {
