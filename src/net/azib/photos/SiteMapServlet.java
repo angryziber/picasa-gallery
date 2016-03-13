694e37a9c2afce5fb3bfd32c918db9df349ab479
Anton Keks <anton@azib.net>
2016-03-13 21:08:10 +0200

    remove unused import

--- src/net/azib/photos/SiteMapServlet.java
+++ src/net/azib/photos/SiteMapServlet.java
@@ -2,7 +2,6 @@ package net.azib.photos;
 
 import com.google.gdata.util.ServiceException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
