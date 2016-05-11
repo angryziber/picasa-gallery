5fafa2ecae050e6d5408d2b3154345e83d2e4b20
Anton Keks <anton@azib.net>
2016-05-11 23:30:48 +0300

    update deployment script to java-7-oracle

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # AppEngine SDK cannot compile jsp files properly under Java 8, so need to force Java 7 :-(
-PATH=/usr/lib/jvm/java-7-openjdk-amd64/bin:$PATH ../appengine-java-sdk-*/bin/appcfg.sh --oauth2 update out/artifacts/picasa_gallery_war_exploded/
+PATH=/usr/lib/jvm/java-7-oracle/bin:$PATH ../appengine-java-sdk*/bin/appcfg.sh --oauth2 update out/artifacts/picasa_gallery_war_exploded/
