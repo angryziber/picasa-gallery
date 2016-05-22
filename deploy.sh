6223d403ef2d6a3bc3b9b58bd0910d02f448388c
Anton Keks <anton@azib.net>
2016-05-23 01:24:33 +0300

    no java7 restriction anymore

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-# AppEngine SDK cannot compile jsp files properly under Java 8, so need to force Java 7 :-(
-PATH=/usr/lib/jvm/java-7-oracle/bin:$PATH ../appengine-java-sdk*/bin/appcfg.sh --oauth2 update out/artifacts/picasa_gallery_war_exploded/
+../appengine-java-sdk*/bin/appcfg.sh --oauth2 update out/artifacts/picasa_gallery_war_exploded/
