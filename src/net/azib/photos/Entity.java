976713bafd55c96e65dd81e729d89338ad551a93
Anton Keks <anton@azib.net>
2016-05-24 23:39:56 +0300

    fix sitemap

--- src/net/azib/photos/Entity.java
+++ src/net/azib/photos/Entity.java
@@ -1,5 +1,7 @@
 package net.azib.photos;
 
+import java.text.SimpleDateFormat;
+
 public class Entity {
   public String title;
   public String description;
@@ -9,6 +11,10 @@ public class Entity {
 
   public String getDescription() {
     return description != null ? description : "";
+  }
+
+  public String getTimestampISO() {
+    return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").format(timestamp);
   }
 
   public Object get(String name) throws NoSuchFieldException, IllegalAccessException {
