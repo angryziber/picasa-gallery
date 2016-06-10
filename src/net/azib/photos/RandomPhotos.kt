94cba6be5d4e015c5a4a6983f04946e5397b73d3
Anton Keks <anton@azib.net>
2016-06-11 02:02:17 +0300

    convert RandomPhotos to Kotlin

---
+++ src/net/azib/photos/RandomPhotos.kt
@@ -0,0 +1,7 @@
+package net.azib.photos
+
+class RandomPhotos(val photos: List<Photo>, val author: String, val album: String) {
+  init {
+    photos.forEach { it.url = it.url?.replace("/s1600/", "/s1920/") }
+  }
+}
