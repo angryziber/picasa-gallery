06c89feaec482385677c3ea00a1d9bcf81da2088
Anton Keks <anton@azib.net>
2016-06-11 02:13:16 +0300

    get rid of !!

--- src/net/azib/photos/AlbumLoader.kt
+++ src/net/azib/photos/AlbumLoader.kt
@@ -6,7 +6,7 @@ import java.lang.Long.parseLong
 import java.util.*
 
 class AlbumLoader : XMLListener<Album> {
-  private var album: Album? = null
+  private lateinit var album: Album
   private var photo: Photo? = null
   private var comment: Comment? = null
 
@@ -23,56 +23,57 @@ class AlbumLoader : XMLListener<Album> {
 
   override fun value(path: String, value: String) {
     when (path) {
-      "name" -> album!!.name = value
-      "title" -> album!!.title = value
-      "subtitle" -> album!!.description = value
-      "icon" -> album!!.thumbUrl = value
-      "timestamp" -> album!!.timestamp = parseLong(value)
-      "nickname" -> album!!.author = value
-      "user" -> album!!.authorId = value
-      "access" -> album!!.isPublic = "public" == value
+      "name" -> album.name = value
+      "title" -> album.title = value
+      "subtitle" -> album.description = value
+      "icon" -> album.thumbUrl = value
+      "timestamp" -> album.timestamp = parseLong(value)
+      "nickname" -> album.author = value
+      "user" -> album.authorId = value
+      "access" -> album.isPublic = "public" == value
       "numphotos" -> {
-        album!!.size = parseInt(value)
-        album!!.photos = ArrayList<Photo>(album!!.size)
+        album.size = parseInt(value)
+        album.photos = ArrayList<Photo>(album.size)
       }
-      "where/Point/pos" -> album!!.geo = GeoLocation(value)
+      "where/Point/pos" -> album.geo = GeoLocation(value)
       "entry/category@term" -> {
         if (value.endsWith("photo")) {
           photo = Photo()
-          album!!.photos.add(photo!!)
+          album.photos.add(photo!!)
         }
         else if (value.endsWith("comment")) {
           comment = Comment()
-          album!!.comments.add(comment!!)
+          album.comments.add(comment!!)
         }
       }
     }
 
-    if (photo != null) {
+    photo?.let { photo ->
       when (path) {
-        "entry/id" -> photo!!.id = value
-        "entry/title" -> photo!!.title = value
-        "entry/summary" -> photo!!.description = value
-        "entry/timestamp" -> photo!!.timestamp = parseLong(value)
-        "entry/width" -> photo!!.width = parseInt(value)
-        "entry/height" -> photo!!.height = parseInt(value)
-        "entry/content@src" -> photo!!.url = value
-        "entry/group/thumbnail@url" -> photo!!.thumbUrl = value
-        "entry/tags/fstop" -> photo!!.exif.fstop = parseFloat(value)
-        "entry/tags/exposure" -> photo!!.exif.exposure = parseFloat(value)
-        "entry/tags/focallength" -> photo!!.exif.focal = parseFloat(value)
-        "entry/tags/iso" -> photo!!.exif.iso = value
-        "entry/tags/model" -> photo!!.exif.camera = value
-        "entry/where/Point/pos" -> photo!!.geo = GeoLocation(value)
+        "entry/id" -> photo.id = value
+        "entry/title" -> photo.title = value
+        "entry/summary" -> photo.description = value
+        "entry/timestamp" -> photo.timestamp = parseLong(value)
+        "entry/width" -> photo.width = parseInt(value)
+        "entry/height" -> photo.height = parseInt(value)
+        "entry/content@src" -> photo.url = value
+        "entry/group/thumbnail@url" -> photo.thumbUrl = value
+        "entry/tags/fstop" -> photo.exif.fstop = parseFloat(value)
+        "entry/tags/exposure" -> photo.exif.exposure = parseFloat(value)
+        "entry/tags/focallength" -> photo.exif.focal = parseFloat(value)
+        "entry/tags/iso" -> photo.exif.iso = value
+        "entry/tags/model" -> photo.exif.camera = value
+        "entry/where/Point/pos" -> photo.geo = GeoLocation(value)
       }
     }
-    else if (comment != null) {
+    
+    comment?.let { comment ->
       when (path) {
-        "entry/content" -> comment!!.text = value
-        "entry/author/name" -> comment!!.author = value
-        "entry/author/thumbnail" -> comment!!.avatarUrl = value
-        "entry/author/user" -> comment!!.authorId = value
-        "entry/photoid" -> comment!!.photoId = value
+        "entry/content" -> comment.text = value
+        "entry/author/name" -> comment.author = value
+        "entry/author/thumbnail" -> comment.avatarUrl = value
+        "entry/author/user" -> comment.authorId = value
+        "entry/photoid" -> comment.photoId = value
       }
     }
   }
