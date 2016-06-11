8bcc6395cbba90238715090557c28624867ed58c
Anton Keks <anton@azib.net>
2016-06-11 12:06:12 +0300

    convert SiteMapServlet to Kotlin

---
+++ src/net/azib/photos/SiteMapServlet.kt
@@ -0,0 +1,28 @@
+package net.azib.photos
+
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import javax.servlet.http.HttpServletResponse.SC_NOT_FOUND
+
+class SiteMapServlet : HttpServlet() {
+  override fun doGet(req: HttpServletRequest, resp: HttpServletResponse) {
+    resp.writer.use { out ->
+      when (req.servletPath) {
+        "/robots.txt" -> {
+          resp.contentType = "text/plain"
+          out.write("Sitemap: http://${req.getHeader("Host")}/sitemap.xml\n" +
+              "User-Agent: *\n" +
+              "Allow: /\n")
+        }
+        "/sitemap.xml" -> {
+          resp.contentType = "text/xml"
+          val picasa = Picasa()
+          req.setAttribute("gallery", picasa.gallery)
+          RequestRouter.render("sitemap", null, req, resp)
+        }
+        else -> resp.sendError(SC_NOT_FOUND)
+      }
+    }
+  }
+}
