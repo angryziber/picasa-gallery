6485b0a14abfe30670be1a74d1159563837e1852
Anton Keks <anton@azib.net>
2016-12-05 22:50:11 +0200

    use fewer nullability

--- src/net/azib/photos/GalleryLoader.kt
+++ src/net/azib/photos/GalleryLoader.kt
@@ -5,7 +5,7 @@ import java.util.*
 
 class GalleryLoader : XMLListener<Gallery> {
   override var result = Gallery()
-  private var album: Album? = null
+  private var album = Album()
   val timestampFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
 
   init {
@@ -20,24 +20,23 @@ class GalleryLoader : XMLListener<Gallery> {
       "nickname" -> result.author = value
       "updated" -> result.timestamp = parseTimestamp(value)
 
-      "entry/id" -> album?.id = value
-      "entry/name" -> album?.name = value
-      "entry/title" -> album?.title = value
-      "entry/summary" -> album?.description = value
-      "entry/nickname" -> album?.author = value
-      "entry/access" -> album?.isPublic = "public" == value
-      "entry/updated" -> album?.timestamp = parseTimestamp(value)
-      "entry/group/thumbnail@url" -> album?.thumbUrl = value + ".jpg"
-      "entry/where/Point/pos" -> album?.geo = GeoLocation(value)
-      "entry/numphotos" -> album?.size = value.toInt()
+      "entry/id" -> album.id = value
+      "entry/name" -> album.name = value
+      "entry/title" -> album.title = value
+      "entry/summary" -> album.description = value
+      "entry/nickname" -> album.author = value
+      "entry/access" -> album.isPublic = "public" == value
+      "entry/updated" -> album.timestamp = parseTimestamp(value)
+      "entry/group/thumbnail@url" -> album.thumbUrl = value + ".jpg"
+      "entry/where/Point/pos" -> album.geo = GeoLocation(value)
+      "entry/numphotos" -> album.size = value.toInt()
     }
   }
 
-  override fun start(path: String) {
-    if ("entry" == path) album = Album()
-  }
-
   override fun end(path: String) {
-    if ("entry" == path) result += album!!
+    if ("entry" == path) {
+      result += album
+      album = Album()
+    }
   }
 }
