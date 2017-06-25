9761ccab655e04f4e3baeecf552ee324994629b2
Anton Keks <anton@azib.net>
2017-06-25 15:42:36 +0300

    refresh access token automatically

--- src/net/azib/photos/OAuth.kt
+++ src/net/azib/photos/OAuth.kt
@@ -2,6 +2,7 @@ package net.azib.photos
 
 import com.github.scribejava.apis.GoogleApi20
 import com.github.scribejava.core.builder.ServiceBuilder
+import com.github.scribejava.core.model.OAuth2AccessToken
 import java.net.HttpURLConnection
 
 object OAuth {
@@ -11,11 +12,29 @@ object OAuth {
         .callback("http://localhost:8080/oauth")
         .build(GoogleApi20.instance())
 
-  private var token = Config.oauthRefreshToken?.let { service.refreshAccessToken(it) }
+  private var expiresAt: Long = 0
+  private val isExpired get() = expiresAt <= System.currentTimeMillis()
+
+  private var token: OAuth2AccessToken? = null
+    get() {
+      if (isExpired) refresh()
+      return field
+    }
+    set(value) {
+      field = value?.apply {
+        expiresAt = System.currentTimeMillis() + expiresIn * 1000 - 10000
+      }
+    }
 
   fun token(code: String) = service.getAccessToken(code).apply {
     token = this
     Config.oauthRefreshToken = refreshToken
+  }
+
+  private fun refresh() {
+    Config.oauthRefreshToken?.let {
+      token = service.refreshAccessToken(it)
+    }
   }
 
   fun authorize(conn: HttpURLConnection) {
