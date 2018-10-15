df9de269f36e38c06b5ae525b9425b64ce7761f2
Anton Keks <anton@azib.net>
2018-10-16 01:22:26 +0300

    fix js escaping

--- src/views/utils.kt
+++ src/views/utils.kt
@@ -5,6 +5,6 @@ val newline = "\r?\n".toRegex()
 operator fun Boolean.div(s: String) = if (this) s else ""
 
 fun String?.escapeHTML() = this?.replace("<", "&lt;") ?: ""
-fun String?.escapeJS() = this?.replace("'", "\'") ?: ""
+fun String?.escapeJS() = this?.replace("'", "\\'") ?: ""
 
 operator fun String?.unaryPlus() = escapeHTML()
