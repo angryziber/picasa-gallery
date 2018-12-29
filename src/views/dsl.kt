82af26bd27e97faad3b0ad07a2034d1fa4ac21ef
Anton Keks <anton@azib.net>
2018-12-30 00:05:30 +0200

    must support nullable receiver

--- src/views/dsl.kt
+++ src/views/dsl.kt
@@ -4,7 +4,7 @@ val newline = "\r?\n".toRegex()
 
 operator fun Boolean.div(s: String) = if (this) s else ""
 
-fun String?.escapeHTML() = this?.replace("<", "&lt;").replace("\"", "&quot;") ?: ""
+fun String?.escapeHTML() = this?.replace("<", "&lt;")?.replace("\"", "&quot;") ?: ""
 fun String?.escapeJS() = this?.replace("'", "\\'") ?: ""
 
 operator fun String?.unaryPlus() = escapeHTML()
