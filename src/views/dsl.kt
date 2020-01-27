29e406ae93a00f553d8a3003a0f7e00572434e71
Anton Keks <anton@azib.net>
2020-01-27 22:57:20 +0200

    escape & first

--- src/views/dsl.kt
+++ src/views/dsl.kt
@@ -5,7 +5,7 @@ val newline = "\r?\n".toRegex()
 operator fun Boolean.div(s: String) = if (this) s else ""
 operator fun Any?.div(s: String) = if (this != null) s else ""
 
-fun String?.escapeHTML() = this?.replace("<", "&lt;")?.replace("\"", "&quot;") ?: ""
+fun String?.escapeHTML() = this?.replace("&", "&amp;")?.replace("<", "&lt;")?.replace("\"", "&quot;") ?: ""
 fun String?.escapeJS() = this?.replace("'", "\\'") ?: ""
 
 operator fun String?.unaryPlus() = escapeHTML()
